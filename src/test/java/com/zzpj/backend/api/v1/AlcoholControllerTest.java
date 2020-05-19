f97addfa2e19255914c35dfa4a28022dd7b47c41
Mateusz Wasilewski <wasilewskimateusz1998@gmail.com>
2020-05-19 15:43:23 +0200

    Add PurchaseController tests

--- src/test/java/com/zzpj/backend/api/v1/AlcoholControllerTest.java
+++ src/test/java/com/zzpj/backend/api/v1/AlcoholControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
