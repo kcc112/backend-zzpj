06458959f338b16e3de4c3a2725de27888169b15
Mateusz Wasilewski <wasilewskimateusz1998@gmail.com>
2020-05-19 15:32:36 +0200

    Add AlcoholController tests

---
+++ src/test/java/com/zzpj/backend/api/v1/AlcoholControllerTest.java
@@ -0,0 +1,98 @@
+package com.zzpj.backend.api.v1;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zzpj.backend.entities.Alcohol;
+import com.zzpj.backend.services.interfaceses.AlcoholServiceLocal;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = AlcoholController.class)
+public class AlcoholControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    AlcoholServiceLocal alcoholService;
+
+
+    @Test
+    void getAll_whenValidInput_thenReturns200 () throws Exception {
+        mockMvc.perform(get("/api/v1/alcohols"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getAll_whenValidInput_thenReturnsAlcoholList() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/alcohols")).andReturn();
+
+        ArrayList<Alcohol> expectedResponseBody = new ArrayList<>();
+        String actualResponseBody = mvcResult.getResponse().getContentAsString();
+
+        assertThat(objectMapper.writeValueAsString(expectedResponseBody))
+                .isEqualToIgnoringWhitespace(actualResponseBody);
+    }
+
+    @Test
+    void getAlcohol_whenValidInput_thenReturns200 () throws Exception {
+        mockMvc.perform(get("/api/v1/alcohols/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getAlcohol_whenValidInput_thenReturnsAlcohol() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/alcohols/1")).andReturn();
+
+        Alcohol expectedResponseBody = new Alcohol();
+        String actualResponseBody = mvcResult.getResponse().getContentAsString();
+
+        assertThat(objectMapper.writeValueAsString(expectedResponseBody))
+                .isEqualToIgnoringWhitespace(actualResponseBody);
+    }
+
+    @Test
+    void add_whenValidInput_thenReturns200 () throws Exception {
+        Alcohol alcohol = new Alcohol();
+        alcohol.setName("Perla");
+        alcohol.setAmount(20);
+        alcohol.setCost(2.5);
+        mockMvc.perform(post("/api/v1/alcohols")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(alcohol))
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    void edit_whenValidInput_thenReturns200 () throws Exception {
+        Alcohol alcohol = new Alcohol();
+        alcohol.setName("Perla");
+        alcohol.setAmount(20);
+        alcohol.setCost(2.5);
+        mockMvc.perform(put("/api/v1/alcohols")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(alcohol))
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    void delete_whenValidInput_thenReturns200 () throws Exception {
+        mockMvc.perform(delete("/api/v1/alcohols/1")).
+                andExpect(status().isOk());
+    }
+
+
+
+}
