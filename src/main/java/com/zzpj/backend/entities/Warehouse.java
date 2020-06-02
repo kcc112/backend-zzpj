f7ca779d76fcf9ce74eee18403a7bf425d59aff7
Szymon Dobrowolski <216747@edu.p.lodz.pl>
2020-06-02 12:58:14 +0200

    [REFACTOR] Warehouse class with annotation getter and setter instead of data

--- src/main/java/com/zzpj/backend/entities/Warehouse.java
+++ src/main/java/com/zzpj/backend/entities/Warehouse.java
@@ -2,22 +2,29 @@ package com.zzpj.backend.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.Min;
 
 @Entity
-@Data
 public class Warehouse {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
     private Long id;
     @OneToOne(mappedBy = "warehouse")
     @JsonIgnore
+    @Getter
+    @Setter
     private Alcohol alcohol;
     @Column
     @Min(0)
+    @Getter
+    @Setter
     private int amount;
 
     public void addAmount(int amount){
