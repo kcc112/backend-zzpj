72adf3011e1572bec540d6b12f91aab737c5da15
dobrucki <dobrucki2@gmail.com>
2020-06-15 16:37:33 +0200

    Add external api for currency converting

--- src/main/java/com/zzpj/backend/BackEndApplication.java
+++ src/main/java/com/zzpj/backend/BackEndApplication.java
@@ -2,12 +2,19 @@ package com.zzpj.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BackEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
