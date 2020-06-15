0405a891a10f42fbea3d917beae9144ef1254cc8
Mateusz Wasilewski <50882488+wasilewskiMateusz@users.noreply.github.com>
2020-06-15 22:22:10 +0200

    Remove issues (#27)

--- src/main/java/com/zzpj/backend/services/implementations/CurrencyService.java
+++ src/main/java/com/zzpj/backend/services/implementations/CurrencyService.java
@@ -3,14 +3,9 @@ package com.zzpj.backend.services.implementations;
 import com.zzpj.backend.services.interfaceses.CurrencyServiceLocal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.List;
 import java.util.Map;
 
 @Service
--- src/main/java/com/zzpj/backend/services/interfaceses/CurrencyServiceLocal.java
+++ src/main/java/com/zzpj/backend/services/interfaceses/CurrencyServiceLocal.java
@@ -1,7 +1,5 @@
 package com.zzpj.backend.services.interfaceses;
 
-import java.util.List;
-
 public interface CurrencyServiceLocal {
     Double convertCurrencies(Double value, String from, String to);
 }
